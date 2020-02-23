39a5f01c47ed4c7a483281eab6d1860a203106ed
ganamuhibudin <gana.muhibudin@gmail.com>
2020-02-23 23:39:08 +0700

    update auth helper

--- helpers/auth.go
+++ helpers/auth.go
@@ -9,8 +9,14 @@ import (
 
 func Authentication(ctx iris.Context) {
 	authHeader := ctx.GetHeader("Authorization")
+	if authHeader == "" {
+		errMsg := "Unauthorized"
+		NewResponse(ctx, iris.StatusInternalServerError, errMsg)
+		return
+	}
+
 	tokenString := strings.Split(authHeader, " ")
-	token, err := jwt.Parse(tokenString[1], func(token *jwt.Token) (interface{}, error) {
+	_, err := jwt.Parse(tokenString[1], func(token *jwt.Token) (interface{}, error) {
 		if jwt.GetSigningMethod("HS256") != token.Method {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -19,15 +25,8 @@ func Authentication(ctx iris.Context) {
 	})
 
 	if err != nil {
-		errMsg := "Error token. Error: " + err.Error()
+		errMsg := "Unauthorized. Error: " + err.Error()
 		NewResponse(ctx, iris.StatusInternalServerError, errMsg)
-		return
-	}
-
-	if token != nil && err == nil {
-		fmt.Println("token verified")
-	} else {
-		NewResponse(ctx, iris.StatusUnauthorized, "Unauthorized")
 		return
 	}
 	ctx.Next()
